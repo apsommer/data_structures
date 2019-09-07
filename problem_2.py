d85ae7f20496ed55b057bed90ec9e2e9906a0e1b
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-06 19:50:22 -0700

    feat: Solve problem 2

--- problem_2.py
+++ problem_2.py
@@ -16,13 +16,72 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
 
-# print all files in this directory
-print (os.listdir("."))
+    # initialize a blank list to hold all files with passed suffix
+    files = []
 
-# check for presence of specific file
-print (os.path.isfile("./ex.py"))
+    # recursive layer
+    def _find_files(suffix, path, files):
 
-# does the specified file end with extension .py?
-print ("./ex.py".endswith(".py"))
+        # all files and directories on this level
+        items = os.listdir(path)
+
+        # loop through all files and directories on this level
+        while len(items) > 0:
+
+            # pop this item off the list and analyze its structure
+            item = items.pop()
+
+            # full path name for this layer
+            sub_path = path + '/' + item
+
+            # directory name, does not include the . separator
+            if '.' not in item:
+
+                # recurisve call into next subdirectory
+                files = _find_files(suffix, sub_path, files)
+
+            # file name, check if it has the passed suffix
+            if item.endswith(suffix):
+
+                # append the full path and filename to the output list
+                files.append(sub_path)
+
+        # return the list up a recursive layer
+        return files
+
+    # start recursion with the passed directory name
+    return _find_files(suffix, path, files)
+
+########## TESTING ##########
+
+# all files with suffix .c in subdirectory p2_testdir/
+print()
+print("all files with suffix .c in subdirectory p2_testdir/")
+print(*find_files(".c", "p2_testdir"), sep = "\n")
+# p2_testdir/t1.c
+# p2_testdir/subdir5/a.c
+# p2_testdir/subdir3/subsubdir1/b.c
+# p2_testdir/subdir1/a.c
+
+# all files with suffix .h in subdirectory p2_testdir/
+print()
+print("all files with suffix .h in subdirectory p2_testdir/")
+print(*find_files(".h", "p2_testdir"), sep = "\n")
+# p2_testdir/t1.h
+# p2_testdir/subdir5/a.h
+# p2_testdir/subdir3/subsubdir1/b.h
+# p2_testdir/subdir1/a.h
+
+# all files with suffix .py in current directory ./
+print()
+print("all files with suffix .py in current directory ./")
+print(*find_files(".py", "."), sep = "\n")
+# ./problem_2.py
+# ./problem_1.py
+
+# all files with suffix .none in current directory ./
+print()
+print("all files with suffix .none in current directory ./")
+print(*find_files(".none", "."), sep = "\n")
+# (no output)

a9d4de4aff3a8f6759b3ea5c1a7550b390afa5f6
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-09 10:38:18 -0700

    feat: Compose intersection algorithm

--- problem_6.py
+++ problem_6.py
@@ -79,8 +79,42 @@ def union(llist_1, llist_2):
 
 # intersection = A and B
 def intersection(llist_1, llist_2):
-    # Your Solution Here
-    pass
+
+    # very similiar to union of A or B
+
+    # hashmap to unique element set
+    hashmap = {}
+
+    # iterate through first linked list
+    node = llist_1.head
+    while node:
+
+        # add the element value to the hashmap
+        hashmap[node.value] = node.value
+        node = node.next
+
+    # now compare each element in the second list against the hashmap
+    # if it exists in the map then save it as this is part of the intersection
+
+    output = LinkedList()
+
+    # iterate through second linked list
+    node = llist_2.head
+    while node:
+
+        # element in list 2 also exists in list 1
+        if node.value in hashmap:
+
+            # append to the output list
+            output.append(node.value)
+
+            # pop this key off the map to ensure element uniqueness in the output list
+            hashmap.pop(node.value)
+
+        # move to next element from head to tail
+        node = node.next
+
+    return output
 
 ########## TESTING ##########
 
@@ -97,8 +131,8 @@ for i in element_2:
 
 print(union(linked_list_1, linked_list_2))
 # 3 -> 2 -> 4 -> 35 -> 6 -> 65 -> 21 -> 32 -> 9 -> 1 -> 11 ->
-# print(intersection(linked_list_1,linked_list_2))
-#
+print(intersection(linked_list_1,linked_list_2))
+# 6 -> 4 -> 21 ->
 
 # test 2
 linked_list_3 = LinkedList()
@@ -113,5 +147,21 @@ for i in element_2:
 
 print (union(linked_list_3,linked_list_4))
 # 3 -> 2 -> 4 -> 35 -> 6 -> 65 -> 23 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 ->
-# print (intersection(linked_list_3,linked_list_4))
-# #
+print (intersection(linked_list_3,linked_list_4))
+# (no output)
+
+# test 3
+linked_list_3 = LinkedList()
+element_1 = [1, 2, 3]
+for i in element_1:
+    linked_list_3.append(i)
+
+linked_list_4 = LinkedList()
+element_2 = []
+for i in element_2:
+    linked_list_4.append(i)
+
+print (union(linked_list_3,linked_list_4))
+# 1 -> 2 -> 3 ->
+print (intersection(linked_list_3,linked_list_4))
+# (no output)

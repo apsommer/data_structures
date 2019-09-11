acc6ff3c7b80c9ad6388d078fe34e4b865965c18
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-10 20:08:08 -0700

    feat: Add get/peek to LRU cache

--- problem_1.py
+++ problem_1.py
@@ -29,8 +29,19 @@ class LRU_Cache:
         self.capacity = capacity
         self.num_nodes = 0
 
-    # remove an item from the cache and return it (pop)
+    # get an item from the cache (peek)
     def get(self, key):
+
+        # catch if the key does not exist
+        if key not in self.hashmap:
+            return -1
+
+        # return the value for this key
+        node = self.hashmap[key]
+        return node.value
+
+    # remove an item from the cache and return it
+    def pop(self, key):
 
         # catch if the key does not exist
         if key not in self.hashmap:
@@ -76,19 +87,24 @@ class LRU_Cache:
             # we are at capacity, head (oldest) must be popped first
             if self.num_nodes == self.capacity:
 
-                # simply move head pointer and update the element counter
+                # pop the head out of the tracking hashmap
+                self.hashmap.pop(self.head.key)
+
+                # move linked list head pointer
                 self.head = self.head.next
+
+                # update the element counter
                 self.num_nodes -= 1
 
             # create a new node with the passed key:value pair
             node = Node(key, value)
 
-            # cache is empty, set this node as both the head and tail
+            # catch empty cache, this node becomes both head and tail
             if self.head == None:
                 self.head = node
                 self.tail = node
 
-            # cache has at least one node, this new node becomes the tail
+            # else the cache has at least one node, and this new node becomes the new tail
             else:
 
                 # save the current tail
@@ -112,49 +128,125 @@ class LRU_Cache:
         node = self.head
 
         # loop through each node and print its key:value pair
+        print()
         while node:
             print("key = " + str(node.key) + " : value = " + str(node.value))
             node = node.next
 
 ########## TESTING ##########
 
-# create cache
+# test 1
+print()
+print("test 1:")
+
+# create a cache
 cache = LRU_Cache()
 
-# add 3 simple elements
+# add 3 items to the cache
 cache.set(1, 1)
 cache.set(2, 2)
 cache.set(3, "apples")
 print()
-print("add 3 simple elements")
+print("add 3 items to the cache")
 cache.print_cache()
 # key = 1 : value = 1
 # key = 2 : value = 2
 # key = 3 : value = apples
 
-# get the 2nd element
+# get the 2nd item
 print()
-print("get the 2nd element")
+print("get the 2nd item")
 print(cache.get(2))
 # 2
 
-# verify 2nd element was removed
+cache.print_cache()
+# key = 1 : value = 1
+# key = 2 : value = 2
+# key = 3 : value = apples
+
+# pop the 2nd item
 print()
-print("verify 2nd element was removed")
+print("pop the 2nd item")
+print(cache.pop(2))
+# 2
+
 cache.print_cache()
 # key = 1 : value = 1
 # key = 3 : value = apples
 
-# add 3 simple elements to reach capacity of 5
+# add 3 more items to the cache to reach capacity of 5
 cache.set(4, 4)
 cache.set(8, "bananas")
 cache.set(12, 42)
 print()
-print("add 3 simple elements to reach capacity of 5")
+print("add 3 more items to the cache to reach capacity of 5")
 cache.print_cache()
 
 # add 1 more element, this exceeds capacity and oldest element (1:1) is removed
 cache.set(9, 9)
 print()
-print("add 1 more element, this exceeds capacity and oldest element (1:1) is removed")
+print("add 1 more item, this exceeds capacity and oldest element (1:1) is removed")
 cache.print_cache()
+
+# test 2
+
+cache = LRU_Cache(5)
+cache.set(1, 1)
+cache.set(2, 2)
+cache.set(3, 3)
+cache.set(4, 4)
+print()
+print("test 2:")
+print(cache.get(1))
+# 1
+print(cache.get(2))
+# 2
+print(cache.get(9))
+# -1
+cache.set(5, 5)
+
+cache.print_cache()
+# key = 1 : value = 1
+# key = 2 : value = 2
+# key = 3 : value = 3
+# key = 4 : value = 4
+# key = 5 : value = 5
+
+cache.set(6, 6)
+
+cache.print_cache()
+# key = 2 : value = 2
+# key = 3 : value = 3
+# key = 4 : value = 4
+# key = 5 : value = 5
+# key = 6 : value = 6
+
+cache.set(7, 7)
+
+cache.print_cache()
+# key = 3 : value = 3
+# key = 4 : value = 4
+# key = 5 : value = 5
+# key = 6 : value = 6
+# key = 7 : value = 7
+
+print(cache.get(1))
+# -1
+print(cache.get(2))
+# -1
+print(cache.get(3))
+# 3
+
+print(cache.pop(3))
+# 3
+print(cache.pop(4))
+# 4
+print(cache.pop(42))
+# -1
+
+cache.print_cache()
+# key = 5 : value = 5
+# key = 6 : value = 6
+# key = 7 : value = 7
+
+#
